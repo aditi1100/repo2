ac0840602f8815485ebe8bb538522a1779631e76
aditi mishra <aditi1100@gmail.com>
2016-09-13 21:38:59 -0700

    question 1 of assignment 3

---
+++ Documents/q1_a3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double f( double g){
+	g=g*g;
+	return g;
+}
+
+
+
+void main(int argc, char* argv[]){
+
+	double f(double gee);
+	double integration;
+	int a=atoi(argv[1]);	
+	int b=atoi(argv[2]);
+
+	int n=atoi(argv[3]);
+	
+	for(int i=0;i<n;i++){
+		integration=f((double)(a+(i+0.5)*((b-a)/n))/((b-a)/n));
+	}
+	printf("\n%f result is ",integration);
+
+}
+
+
+
+
+
+
